89e7e6fa3a7972b1202744c71d08a34b12af9ee9
BREAKMANS <breakmanspintorosa@gmail.com>
2024-09-02 18:45:55 -0300

    servirdor

--- TCC - BACKEND/backend/Fontend/API/api.js
+++
@@ -1,6 +0,0 @@
-async function localhost() {
-    const response = await
-    fetch("http://localhost:8080/gravar");
-    const localhost = await response.json();
-    console.log(localhost);
-}
--- TCC - BACKEND/backend/database/conexao.js
+++
@@ -1,6 +0,0 @@
-const knex = require('knex');
-const configuracao = require('../knex');
-
-const conexao = knex(configuracao);
-
-module.exports = conexao;
